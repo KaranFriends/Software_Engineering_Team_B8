99e056bc23ebc4b2b291bcd3a1f643f34c007418
Jamie <41702710+ProfitSword@users.noreply.github.com>
2022-04-15 22:47:57 -0400

    updated database table code

--- cinema_project/app.py
+++ cinema_project/app.py
@@ -8,13 +8,18 @@ from flask_bcrypt import Bcrypt
 from datetime import datetime, timedelta
 import emailService
 import random
-from model.model import User, Card, Ticket, Review, Movie, MovieCategory, Show, Showroom, Booking, TicketBooking, TicketPrice, Img
+from model.model import User
+#from model.model import User, Card #Ticket, Review, Movie, MovieCategory, Show, Showroom, Booking, TicketBooking, TicketPrice, Img
 from model.LoginForm import LoginForm
 from model.RegistrationForm import RegistrationForm
 from model.UserEditForm import UserEditForm
 from model.ChangePasswordEnterEmail import ChangePasswordEnterEmail
 from model.ChangePasswordVerifyOTP import ChangePasswordVerifyOTP
 from model.ChangePasswordNewPassword import ChangePasswordNewPassword
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -31,6 +36,133 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+
+class User(db.Model, UserMixin):
+     id = db.Column(db.Integer, primary_key=True, nullable=False) #note: in sqlite, primary key column is not nullable by default
+     email = db.Column(db.String(30), nullable=False)
+     password = db.Column(db.String(80), nullable=False)
+     first_name = db.Column(db.String(10), nullable=False)
+     last_name = db.Column(db.String(10), nullable=False)
+     dob = db.Column(db.String(8), nullable=False)
+     addressline1 = db.Column(db.String(20), nullable=False)
+     addressline2 = db.Column(db.String(20), nullable=False)
+     city = db.Column(db.String(8), nullable=False)
+     state = db.Column(db.String(8), nullable=False)
+     zip = db.Column(db.String(8), nullable=False)
+     country = db.Column(db.String(8), nullable=False)
+     cards = db.relationship('Card', backref='user', lazy=True)
+     promotions = db.Column(db.String(1))
+     user_type = db.Column(db.Integer, nullable=False)
+     reviews = db.relationship('Review', backref='user', lazy=True)
+     bookings = db.relationship('Booking', backref='user', lazy=True)
+
+class Card(db.Model, UserMixin):
+     id = db.Column(db.Integer, primary_key=True, nullable=False)
+     cardNumber = db.Column(db.String(12), nullable=False)
+     cardName = db.Column(db.String(15), nullable=False)
+     cvv = db.Column(db.String(3), nullable=False)
+     expirationDate = db.Column(db.String(8), nullable=False)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+         nullable=False)
+     bookings = db.relationship('Booking', backref='card', lazy=True)
+
+class Ticket(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         ticket_category_id=db.Column(db.Integer, nullable=False)
+         ticket_category_quantity=db.Column(db.Integer, default=0, nullable=False)
+         seat_no=db.Column(db.Integer, nullable=False)
+         ticketBookings = db.relationship('TicketBooking', backref='ticket', lazy=True)
+
+class Review(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         review_comment = db.Column(db.String(12), nullable=False)
+         review_rating = db.Column(db.Integer, nullable=False)
+         user_ID=db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+class Movie(db.Model, UserMixin):
+          id = db.Column(db.Integer, primary_key=True, nullable=False)
+          movie_title = db.Column(db.String(12), nullable=False) #char(50)
+          movie_director_name = db.Column(db.String(12), nullable=False) #char(50)
+          movie_cast_name = db.Column(db.String(12), nullable=False) #char(50)
+          movie_producer_name = db.Column(db.String(12), nullable=False)
+          movie_synopsis = db.Column(db.String(12), nullable=False)
+          movie_status = db.Column(db.Integer, nullable=False) 
+          movie_trailer = db.Column(db.Integer, nullable=False) 
+          movie_picture = db.Column(db.Integer, nullable=False)
+          movie_video = db.Column(db.Integer, nullable=False) 
+          category_ID=db.Column(db.Integer, db.ForeignKey('moviecategory.id'), nullable=False)
+          shows = db.relationship('Show', backref='movie', lazy=True)
+
+
+
+class Moviecategory(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         category_name = db.Column(db.String(12), nullable=False)
+         movies = db.relationship('Movie', backref='moviecategory', lazy=True)
+
+
+class Show(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         show_date = db.Column(db.Date, nullable=False)
+         show_time = db.Column(db.Time, nullable=False)
+         movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"), nullable=False)
+         showroom_id = db.Column(db.Integer, db.ForeignKey("showroom.id"), nullable=False)
+         bookings = db.relationship('Booking', backref='show', lazy=True)
+         seatAvail= db.relationship('Seatavail', backref='show', lazy=True)
+
+
+class Showroom(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         total_seats = db.Column(db.Integer, nullable=False)
+         shows = db.relationship('Show', backref='showroom', lazy=True)
+         seats=db.relationship('Seat', backref='showroom', lazy=True)
+
+class Seat(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    seatAvail=db.relationship('Seatavail', backref='seat', lazy=True)
+
+class Seatavail(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True, nullable=False)
+    show_ID=db.Column(db.Integer, db.ForeignKey('show.id'), nullable=False)
+    seat_ID= db.Column(db.Integer, db.ForeignKey('seat.id'), nullable=False)
+    available=db.Column(db.Integer, nullable=False) #0 for false, 1 for true, sqlite does not have boolean data type
+
+
+class Booking(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+         show_id = db.Column(db.Integer, db.ForeignKey("show.id"), nullable=False)
+         card_id = db.Column(db.Integer, db.ForeignKey("card.id"), nullable=False)
+         booking_date = db.Column(db.Date, nullable=False)
+         booking_time = db.Column(db.Time, nullable=False)
+         booking_price = db.Column(db.Integer, nullable=False)
+         ticketBookings = db.relationship('TicketBooking', backref='booking', lazy=True)
+
+
+class TicketBooking(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         booking_id = db.Column(db.Integer, db.ForeignKey("booking.id"), nullable=False)
+         ticket_id = db.Column(db.Integer, db.ForeignKey("ticket.id"), nullable=False)
+
+
+class TicketPrice(db.Model, UserMixin):
+         id = db.Column(db.Integer, primary_key=True, nullable=False)
+         ticket_price = db.Column(db.Integer, nullable=False)
+         ticket_category_name = db.Column(db.Enum, nullable=False)
+
+
+# # table for storing images directly
+class Img(db.Model, UserMixin):
+          id=db.Column(db.Integer, primary_key=True, nullable=False)
+          img=db.Column(db.Text, unique=True, nullable=False)
+          name=db.Column(db.String(12), nullable=False )
+          mimeType=db.Column(db.String(20), nullable=False)
+
+
+
+
 
 
 # @login_manager.user_loader
